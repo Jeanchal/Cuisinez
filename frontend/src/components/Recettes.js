627ce1299fec2dfb8b7d0b2e87b8099287353935
Jean Chalret <jecdr_75@hotmail.fr>
2021-12-09 21:27:48 +0100

    correction recettes acceuil

--- frontend/src/components/Recettes.js
+++ frontend/src/components/Recettes.js
@@ -1,22 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Recettes = ({ result, recette }) => {
   // const result = data.filter((item) => item.name.includes(search));
+  const [like, setLike] = useState(false);
+
+  function activLike() {
+    like === false ? setLike(true) : setLike(false);
+  }
 
   return (
     <div className="recette">
       <img src="../img/boeuf_bourguignon.jpg" alt="recette" />
       <div className="recette_text">
         <h3>{recette.name}</h3>
-        <div>temps de préparation: {recette.time_cooking}</div>
-        <br />
-        <div>Ingédients:</div>
-        <ul>
-          <li>{recette.ingredients[0]}</li>
-          <li>{recette.ingredients[1]}</li>
-          <li>{recette.ingredients[2]}</li>
-          <li>{recette.ingredients[3]}</li>
-        </ul>
+        <div id="icons_container">
+          <div id="stars">
+            <i className="fas fa-star"></i>
+            <i className="fas fa-star"></i>
+            <i className="fas fa-star"></i>
+            <i className="fas fa-star"></i>
+            <i className="fas fa-star"></i>
+          </div>
+          <div
+            id="coeur"
+            onClick={activLike}
+            className={like === true ? "red_color" : null}
+          >
+            <i className="fas fa-heart"></i>
+          </div>
+        </div>
       </div>
     </div>
   );
