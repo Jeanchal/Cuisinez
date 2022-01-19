81aecc3a5717f0618c45b865b4b27cf196e71a1f
Jean Chalret <jecdr_75@hotmail.fr>
2022-01-19 18:59:59 +0100

    profil

--- frontend/src/pages/Profil.js
+++ frontend/src/pages/Profil.js
@@ -3,9 +3,20 @@ import Header from "../components/Header";
 
 const Profil = () => {
   return (
-    <div className="page">
+    <div className="page" id="profil">
       <Header />
       <h1>Profil</h1>
+      <figure>
+        <img src="./img/profil.jpg" alt="" />
+      </figure>
+      <br />
+      <div id="infos-container">
+        <p>Pseudo: JM-Cuiz78</p>
+        <p>Prénom: Jean-Michel</p>
+        <p>Nom: Caneloni</p>
+        <p>Âge: 31 ans</p>
+        <p>Niveau: Aprenti</p>
+      </div>
     </div>
   );
 };
